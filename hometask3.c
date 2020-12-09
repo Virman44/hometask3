3c47aeae13df7c7b74893f027a70ee73d4a21698
Virman44 <56293360+Virman44@users.noreply.github.com>
2020-12-09 16:36:26 +0300

    Update hometask3.c

--- hometask3.c
+++ hometask3.c
@@ -11,46 +11,34 @@ int main(void) {
 	int n, n1, n2;
 	srand(time(NULL));
 	n = 3 + rand() % 20;
+	n1 = 3 + rand() % 20;
 	a = (int**)malloc(n * sizeof(int*));
+	b = (int**)malloc(n1 * sizeof(int*));
 
 	for (int i = 0; i < n; i++) {
 		a[i] = (int*)malloc(n * sizeof(int));
+		b[i] = (int*)malloc(n1 * sizeof(int));
 	}
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			a[i][j] = rand() % 2;
-		}
-	}
-	
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
 			printf("%d ", a[i][j]);
 		}
 		printf("\n");
 	}
-
+	
 	printf("\n");
 
-	n1 = 3 + rand() % 20;
-	b = (int**)malloc(n1 * sizeof(int*));
-
-	for (int i = 0; i < n1; i++) {
-		b[i] = (int*)malloc(n1 * sizeof(int));
-	}
 
 	for (int i = 0; i < n1; i++) {
 		for (int j = 0; j < n1; j++) {
 			b[i][j] = rand() % 2;
-		}
-	}
-
-	for (int i = 0; i < n1; i++) {
-		for (int j = 0; j < n1; j++) {
 			printf("%d ", b[i][j]);
 		}
 		printf("\n");
 	}
+
 
 	if (n >= n1) {
 		n2 = n1;
